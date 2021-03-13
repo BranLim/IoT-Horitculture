3d10752111ad5855d65a7dfd7442e0e57fd44916
Brandon Lim <limws.brandon@gmail.com>
2021-03-14 01:45:38 +0800

    Display moisture info on button press. Improve responsiveness.

--- SoilMoistureSensor.ino
+++ SoilMoistureSensor.ino
@@ -8,12 +8,23 @@
 
 #include "Arduino_SensorKit.h"
 
+#define button 4
+#define sensorVccPin 7
+#define alarmVccPin 5
+
 int signalPin = A0;
-int sensorVccPin = 7;
-int alarmVccPin = 5;
-int buzzReminder = 3;
+
 bool moistureGood = false;
 int moistureLevel = 0;
+unsigned long displayActive = 0;
+bool activateDisplay = false;
+unsigned long nonBlockingDelay = 3500;
+unsigned long buzzerActive = 0;
+unsigned long buzzerDelay = 1500;
+unsigned long nextBuzzerActive = 1800000;
+bool buzzerGoodToGo = true;
+unsigned long nextBuzzer = 0;
+
 
 void setup()
 {
@@ -24,6 +35,8 @@ void setup()
 
   pinMode(alarmVccPin, OUTPUT); //Enable D#8 with power
   noTone(alarmVccPin); //Set the buzzer voltage low and make no noise
+
+  pinMode(button, INPUT);
 
   if (Oled.begin())
   {
@@ -39,16 +52,41 @@ void setup()
 void loop()
 {
   getSoilMoistureLevel();
-  displayMoistureLevel();
+  if (isButtonPressed())
+  {
+    activateDisplay = true;
+    displayActive = millis();
+  }
+  if (activateDisplay)
+  {
+    Serial.println("Activating display");
+    Oled.setPowerSave(0);
+    displayMoistureLevel();
+  }
   raiseAlarm();
   Serial.print("Soil Moisture = ");
   Serial.println(moistureLevel);
-  delay(1000UL); //Delay for 30 minutes before doing another check.
+  if (activateDisplay && millis() > displayActive + nonBlockingDelay)
+  {
+    Serial.println("Deactivating display");
+    Oled.setPowerSave(1);
+    activateDisplay = false;
+  }
+  delay(500);
+}
+
+bool isButtonPressed()
+{
+  int buttonState = digitalRead(button);
+  if (buttonState == HIGH)
+  {
+    return true;
+  }
+  return false;
 }
 
 void displayMoistureLevel()
 {
-  
   Oled.setCursor(0, 48);
   Oled.print("Soil Moisture = ");
   Oled.setCursor(0, 28);
@@ -58,8 +96,6 @@ void displayMoistureLevel()
     Oled.print("   ");
   }
   Oled.refreshDisplay();
-  //delay(1000);
-
 }
 
 void getSoilMoistureLevel()
@@ -73,62 +109,61 @@ void getSoilMoistureLevel()
 
 void raiseAlarm()
 {
-  int buzzCount = 0;
   if (moistureLevel < 50)
   {
     return;
   }
-
+  if (!buzzerGoodToGo && millis() >= buzzerActive + nextBuzzerActive)
+  {
+    Serial.println("Buzzer good to go again!");
+    buzzerGoodToGo = true;
+  }
   if (moistureLevel <= 600)
   {
     if (moistureGood)
     {
       moistureGood = false;
     }
-
-    while (buzzCount < buzzReminder)
+    if (buzzerGoodToGo)
     {
-
-      if (moistureLevel < 400)
-      {
-        buzzUrgent();
-      }
-      else
-      {
-        buzzGentle();
-      }
-
-      buzzCount++;
+      Serial.println("Buzzer good to go!");
+      buzzerActive = millis();
+      
+      Serial.print("Buzz ");
+      buzzUrgent();
+      
+      Serial.println("Buzzer stop!");
+      buzzerGoodToGo = false;
     }
     return;
   }
   if (moistureLevel >= 850 && !moistureGood)
   {
     moistureGood = true;
-    buzzGood();
+    if (buzzerGoodToGo)
+    {
+      buzzerActive = millis();
+      buzzGood();
+    }
   }
 }
 
-void buzzGentle()
-{
-  tone(alarmVccPin, 60);
-  delay(1500);
-  noTone(alarmVccPin);
-  delay(1500);
-}
 
 void buzzUrgent()
 {
   tone(alarmVccPin, 85);
-  delay(1000);
+  delay(250);
   noTone(alarmVccPin);
-  delay(1000);
+  delay(250);
+  tone(alarmVccPin, 85);
+  delay(250);
+  noTone(alarmVccPin);
+  delay(250);
 }
 
 void buzzGood()
 {
   tone(alarmVccPin, 85);
-  delay(1000);
-  noTone(alarmVccPin);
   delay(500);
+  noTone(alarmVccPin);
 }
