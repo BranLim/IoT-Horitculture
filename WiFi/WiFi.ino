24feafcdce536af26c9ae958ba9e0d50160f1f42
Brandon Lim <limws.brandon@gmail.com>
2021-04-11 23:54:42 +0800

    Established connection to WiFi based on SSID and PWD

--- WiFi/WiFi.ino
+++ WiFi/WiFi.ino
@@ -8,11 +8,13 @@ const byte txPin = 3;
 SoftwareSerial wifiSerial(rxPin, txPin); //PIN 2 to receive data from WiFi, PIN 3 to transmit data to WiFi module
 #endif
 
+const char* wifiName = "";
+const char* wifiPassword = "";
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
@@ -20,35 +22,20 @@ void setup() {
   wifiSerial.begin(9600);
   WiFi.init(&wifiSerial);
 
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(wifiName, wifiPassword);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print("*");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address is: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
-  Serial.println();
-  Serial.println("Scanning available networks ... ");
 
-  printNetworkScanResult();
-  delay(10000);
-}
-
-void printNetworkScanResult()
-{
-  int numSsid = WiFi.scanNetworks();
-  while (numSsid == -1)
-  {
-    Serial.println("Couldn't get a WiFi connection");
-    delay(3000);
-    numSsid = WiFi.scanNetworks();
-  }
-
-
-  Serial.print("Number of available networks: ");
-  Serial.println(numSsid);
-
-  for (int i = 0; i < numSsid; i++)
-  {
-    Serial.print(i + 1);
-    Serial.print(")");
-    Serial.print(WiFi.SSID(i));
-    Serial.println();
-  }
 }
